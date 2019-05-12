e19480d376c3d0e694ae1333b8f000d2964ae00a
Tom Roar Furunes <tom.roar@halloverden.no>
2019-05-13 00:35:41 +0200

    ADD: webapi_no holidays.go

---
+++ pkg/webapi_no/holidays.go
@@ -0,0 +1,58 @@
+package webapi_no
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+const holidaysUrl string = "https://webapi.no/api/v1/holidays/"
+
+type Holidays struct {
+	Data []Holiday `json:"data"`
+}
+
+type Holiday struct {
+	Date        time.Time `json:"date"`
+	Description string    `json:"description"`
+}
+
+func NewHolidays(year int) (*Holidays, error) {
+	h := new(Holidays)
+
+	res, err := http.Get(holidaysUrl + strconv.Itoa(year))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		body, _ := ioutil.ReadAll(res.Body)
+		return h, fmt.Errorf("bad response code. got %d. body: %s", res.StatusCode, body)
+	}
+
+	err = json.NewDecoder(res.Body).Decode(h)
+	if err != nil {
+		return h, err
+	}
+
+	return h, nil
+}
+
+func NewHolidaysMulti(years []int) (*Holidays, error) {
+	h := new(Holidays)
+
+	for _, year := range years {
+		holidays, err := NewHolidays(year)
+		if err != nil {
+			return h, err
+		}
+
+		h.Data = append(h.Data, holidays.Data...)
+	}
+
+	return h, nil
+}
